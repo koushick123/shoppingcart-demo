70945bfed7f6c8445412d59f799a565822a2f73d
Koushick Suriyanarayanan <skoushicksuri@gmail.com>
2019-04-17 22:06:26 +0530

    Change Main class and fix maven build issues

--- src/main/java/com/shoppingcart/anzdemo/services/ShoppingCartMain.java
+++ src/main/java/com/shoppingcart/anzdemo/services/ShoppingCartMain.java
@@ -6,7 +6,7 @@ public class ShoppingCartMain {
 		// TODO Auto-generated method stub
 
 		if(args == null || args.length == 0){
-			System.out.println("Please enter an application name.. customer, inventory or checkout");
+			System.out.println("Please enter an application name.. registration, customer, inventory or checkout");
 		}
 		else{
 			for(int i=0;i<args.length;i++){
@@ -17,11 +17,16 @@ public class ShoppingCartMain {
 				case "inventory":
 					InventoryServer.main(args);
 					break;
+				case "checkout":
+					CheckoutOrderServer.main(args);
+					break;
+				case "registration":
+					EurekaRegistrationServer.main(args);
+					break;
 					default:
-						
+						System.out.println("Please enter any of 4 application names (registration, customer, inventory or checkout");
 				}
 			}
 		}
 	}
-
 }
